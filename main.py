7f250ae0ee3baa3ea32804e0a32b508cc6ee0a98
Travis Newton <travis@newtonnet.net>
2021-04-24 19:38:45 -0400

    not working, but some changes

--- main.py
+++ main.py
@@ -2,18 +2,34 @@ import discord
 import os
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
-token=os.getenv('TOKEN')
+token = os.getenv("TOKEN")
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print(f"{client.user} has connected to Discord.")
+
+
 async def on_message(message):
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+    message_content = message.content.lower()
+    if "flip a coin" in message_content:
+        rand_int = random.randint(0, 1)
+        if rand_int == 0:
+            results = "Heads"
+        else:
+            results = "Tails"
+        await message.channel.send(results)
 
 
 client.run(token)
