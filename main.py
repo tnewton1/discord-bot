9e97f548bf5d0a9661918d3c9826b87cb50bd825
Travis Newton <travis@newtonnet.net>
2021-04-24 18:52:34 -0400

    updates

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import discord
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
+token=os.getenv('TOKEN')
+
 client = discord.Client()
 
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord.")
+async def on_message(message):
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run(token)
